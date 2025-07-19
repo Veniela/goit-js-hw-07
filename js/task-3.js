ce553ae2a5fd81e726ddc787eb7800b7f00ec751
Evelina Kobyletska <evelina.kobyletska@gmail.com>
2025-07-19 07:39:36 +0200

    add all changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,35 +1,10 @@
-// Задача 3. Конструктор рядків
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-class StringBuilder {
-  #value;
+input.addEventListener("input", () => {
+  const words = input.value.split(" ");
+  const filtered = words.filter(word => word !== "");
+  const cleanedValue = filtered.join(" ");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+  output.textContent = cleanedValue !== "" ? cleanedValue : "Anonymous";
+});
